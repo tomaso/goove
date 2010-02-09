f92bbf56f67d7c111d7006bf7b0834bf5871d079
tomas <tomas@97c0cabc-8e02-0410-a4d9-a07fbdfa71f1>
2010-02-09 16:50:03 +0000

    basic node attritbutes added
    git-svn-id: svn+ssh://jikos.cz/home/tomas/svnrepo/devel/python/django/goove@319 97c0cabc-8e02-0410-a4d9-a07fbdfa71f1

--- trq/models.py
+++ trq/models.py
@@ -2,13 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Node(models.Model):
+    ordering = ['name']
     name = models.CharField(verbose_name="Node name", max_length=30)
     np = models.IntegerField(verbose_name="Number of job slots")
     properties = models.ManyToManyField('NodeProperty')
+    state = models.ManyToManyField('NodeState')
+    subcluster = models.ForeignKey('SubCluster')
 
     def get_nodeproperties(self):
     	return ",".join([ i.name for i in self.properties.all() ])
     get_nodeproperties.short_description = "Node properties"
+
+    def get_nodestates(self):
+    	return ",".join([ i.name for i in self.state.all() ])
+    get_nodestates.short_description = "Node state"
 
     def __unicode__(self):
     	return self.name
@@ -19,3 +26,18 @@ class NodeProperty(models.Model):
 
     def __unicode__(self):
     	return self.name
+
+class NodeState(models.Model):
+    name = models.CharField(verbose_name="State name", max_length=30)	
+    description = models.CharField(verbose_name="Description", max_length=300)
+
+    def __unicode__(self):
+    	return self.name
+
+class SubCluster(models.Model):
+    name = models.CharField(verbose_name="Subcluster name", max_length=30)	
+    
+    def __unicode__(self):
+    	return self.name
+
+# vi:ts=4:sw=4:expandtab
