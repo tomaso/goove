6fcba9aac2e90428462b81b56512cc1ae07296b5
tomas <tomas@97c0cabc-8e02-0410-a4d9-a07fbdfa71f1>
2010-02-14 08:35:13 +0000

    Import from accounting logs.
    git-svn-id: svn+ssh://jikos.cz/home/tomas/svnrepo/devel/python/django/goove@324 97c0cabc-8e02-0410-a4d9-a07fbdfa71f1

--- trq/models.py
+++ trq/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Node(models.Model):
-    name = models.CharField(verbose_name="Node name", max_length=30)
-    np = models.IntegerField(verbose_name="Number of job slots")
+    name = models.CharField(verbose_name="node name", max_length=30)
+    np = models.IntegerField(verbose_name="number of job slots")
     properties = models.ManyToManyField('NodeProperty')
     state = models.ManyToManyField('NodeState')
     subcluster = models.ForeignKey('SubCluster')
@@ -13,25 +13,28 @@ class Node(models.Model):
 
     def get_nodeproperties(self):
     	return ",".join([ i.name for i in self.properties.all() ])
-    get_nodeproperties.short_description = "Node properties"
+    get_nodeproperties.short_description = "node properties"
 
     def get_nodestates(self):
     	return ",".join([ i.name for i in self.state.all() ])
-    get_nodestates.short_description = "Node state"
+    get_nodestates.short_description = "node state"
 
     def __unicode__(self):
     	return self.name
 
 class NodeProperty(models.Model):
-    name = models.CharField(verbose_name="Property name", max_length=50)
-    description = models.CharField(verbose_name="Description", max_length=300)
+    name = models.CharField(verbose_name="property name", max_length=50)
+    description = models.CharField(max_length=300)
 
     def __unicode__(self):
     	return self.name
 
+    class Meta:
+        verbose_name_plural = "node properties"
+
 class NodeState(models.Model):
     name = models.CharField(verbose_name="State name", max_length=30)	
-    description = models.CharField(verbose_name="Description", max_length=300)
+    description = models.CharField(max_length=300)
 
     def __unicode__(self):
     	return self.name
@@ -42,8 +45,8 @@ class SubCluster(models.Model):
     	return self.name
 
 class Job(models.Model):
-    jobid = models.IntegerField()
-    server = models.ForeignKey('TorqueServer')
+    jobid = models.IntegerField(db_index=True, editable=False)
+    server = models.ForeignKey('TorqueServer', editable=False)
     job_owner = models.ForeignKey('User', null=True)
     cput = models.IntegerField('CPU time in seconds', null=True)
     walltime = models.IntegerField('Wall time in seconds', null=True)
@@ -55,25 +58,30 @@ class Job(models.Model):
     qtime = models.DateTimeField('qtime', null=True)
     etime = models.DateTimeField('etime', null=True)
     start_time = models.DateTimeField('start_time', null=True)
+    comp_time = models.DateTimeField('comp_time', null=True)
+
+    class Meta:
+        ordering = ['jobid']
+        unique_together = (('jobid', 'server'),)
     
 class TorqueServer(models.Model):
-    name = models.CharField(verbose_name="Torque server name", max_length=100)
+    name = models.CharField(verbose_name="torque server name", max_length=100)
     def __unicode__(self):
     	return self.name
 
 class User(models.Model):
-    name = models.CharField(verbose_name="User name", max_length=100)
+    name = models.CharField(verbose_name="user name", max_length=100)
     def __unicode__(self):
     	return self.name
 
 class JobState(models.Model):
-    name = models.CharField(verbose_name="Job state name", max_length=100)
-    shortname = models.CharField(verbose_name="Abbreviation", max_length=1)
+    name = models.CharField(verbose_name="job state name", max_length=100)
+    shortname = models.CharField(verbose_name="abbreviation", max_length=1)
     def __unicode__(self):
     	return self.name
 
 class Queue(models.Model):
-    name = models.CharField(verbose_name="Queue name", max_length=100)
+    name = models.CharField(verbose_name="queue name", max_length=100)
     def __unicode__(self):
     	return self.name
 
