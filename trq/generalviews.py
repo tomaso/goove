0b454f549d300d938dcf7644c3a3b330645be148
tomas <tomas@97c0cabc-8e02-0410-a4d9-a07fbdfa71f1>
2010-03-07 14:49:30 +0000

    Some more initial data for our farm. Better first overview.
    git-svn-id: svn+ssh://jikos.cz/home/tomas/svnrepo/devel/python/django/goove@362 97c0cabc-8e02-0410-a4d9-a07fbdfa71f1

--- trq/generalviews.py
+++ trq/generalviews.py
@@ -5,12 +5,25 @@ from models import SubCluster
 from models import NodeProperty
 from models import NodeState
 from models import Job
+from models import User
+from models import Queue
+from models import TorqueServer
+
+
 from django import forms
 
 def overview(request):
+    info = []
+    for c in (TorqueServer, SubCluster, Node, NodeProperty, User, Queue, Job):
+        item = {}
+        item['name'] = c.get_overview_name()
+        item['count'] = c.objects.all().count()
+        item['url'] = c.get_overview_url()
+        info.append(item)
+
     return render_to_response(
         'trq/overview.html', 
-        {}
+        { 'info': info }
         )
 
 # vi:ts=4:sw=4:expandtab
