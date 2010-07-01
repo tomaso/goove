d7560629496fd69e68adb65a7518716000c9d144
tomas <tomas@97c0cabc-8e02-0410-a4d9-a07fbdfa71f1>
2010-07-01 06:37:59 +0000

    User detail page enriched with a short summary.
    git-svn-id: svn+ssh://jikos.cz/home/tomas/svnrepo/devel/python/django/goove@414 97c0cabc-8e02-0410-a4d9-a07fbdfa71f1

--- trq/userviews.py
+++ trq/userviews.py
@@ -10,8 +10,13 @@ from models import JobState
 from models import User
 from models import Group
 from models import GridUser
+from helpers import getJobState
+from datetime import date
+from django.db.models import Sum,Avg,Count
 
-class UserSelectForm(forms.Form):
+from common_forms import CommonUserForm
+
+class UserSelectForm(CommonUserForm):
     user = forms.ChoiceField(
         label="user",
         initial=User.objects.all()[0],
@@ -29,6 +34,7 @@ def users_overview(request):
 
 def user_detail(request, username=None):
     user_form = UserSelectForm()
+    summary = []
 
     if not request.POST and not username:
         detailuser = None
@@ -39,10 +45,44 @@ def user_detail(request, username=None):
     if detailuser:
         user_form.data['user'] = detailuser.pk
         user_form.is_bound = True
+    if request.POST and request.POST.has_key('summary'):
+        user_form.data['summary'] = request.POST['summary']
+        user_form.is_bound = True
+        if request.POST['summary']:
+            summary.append(('Running jobs', 
+                Job.objects.filter(job_state=getJobState('R'), job_owner=detailuser).count()
+                ))
+            dtoday = date.today()
+            summary.append(('Jobs completed today',
+                Job.objects.filter(
+                    job_state=getJobState('C'), 
+                    job_owner=detailuser,
+                    comp_time__gte=dtoday.isoformat()
+                    ).count()
+                ))
+            summary.append(('Jobs completed this week',
+                Job.objects.filter(
+                    job_state=getJobState('C'), 
+                    job_owner=detailuser,
+                    comp_time__gte=date.fromordinal(dtoday.toordinal()-dtoday.weekday())
+                    ).count()
+                ))
+            msummary = Job.objects.filter(
+                    job_state=getJobState('C'), 
+                    job_owner=detailuser,
+                    comp_time__gte=date(dtoday.year, dtoday.month, 1)
+                    ).aggregate(Sum('cput'), Sum('walltime'), Avg('efficiency'), Count('pk'))
+
+            summary.append(('Jobs completed this month',msummary['pk__count']))
+            summary.append(('CPU time for this month',msummary['cput__sum']))
+            summary.append(('Wall time for this month',msummary['walltime__sum']))
+            summary.append(('Month average efficiency',msummary['efficiency__avg']))
+        
 
     return render_to_response(
         'trq/user_detail.html',
-        {'user':detailuser, 'user_form':user_form}
+        {'user':detailuser, 'user_form':user_form,
+        'summary':summary}
         )
 
 # vi:ts=4:sw=4:expandtab
