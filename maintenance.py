7f3af5cd946daf85a0914ffdb5e2360535b5c8b9
Tomas Kouba <tomas@jikos.cz>
2010-07-18 10:49:51 +0200

    Missing imports added.

--- maintenance.py
+++ maintenance.py
@@ -1,3 +1,4 @@
+from goove.trq.helpers import getJobState, getQueue, getNode, getTorqueServer, getUser, getGroup, getSubmitHost
 from goove.trq.models import JobSlot, Node, NodeProperty, NodeState, SubCluster, Job, RunningJob, TorqueServer, GridUser, User, Group, JobState, Queue, AccountingEvent
 from django.db.models import Avg, Max, Min, Count
 from goove.trq.helpers import LOG_ERROR,LOG_WARNING,LOG_INFO,LOG_DEBUG,log
