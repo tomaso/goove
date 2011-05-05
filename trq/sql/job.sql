157d13b107ef910d60f7185f7ea720f1699b02ad
Tomas Kouba <tomas@jikos.cz>
2011-05-05 19:48:27 +0200

    Default table engine is now innodb.

--- trq/sql/job.sql
+++ trq/sql/job.sql
@@ -1,1 +1,24 @@
+ALTER TABLE trq_accountingevent ENGINE=INNODB;
+ALTER TABLE trq_accountingevent ENGINE=INNODB;
+ALTER TABLE trq_globalconfiguration ENGINE=INNODB;
+ALTER TABLE trq_griduser ENGINE=INNODB;
+ALTER TABLE trq_group ENGINE=INNODB;
+ALTER TABLE trq_job ENGINE=INNODB;
+ALTER TABLE trq_job_jobslots ENGINE=INNODB;
+ALTER TABLE trq_jobslot ENGINE=INNODB;
+ALTER TABLE trq_jobstate ENGINE=INNODB;
+ALTER TABLE trq_node ENGINE=INNODB;
+ALTER TABLE trq_node_properties ENGINE=INNODB;
+ALTER TABLE trq_node_state ENGINE=INNODB;
+ALTER TABLE trq_nodelink ENGINE=INNODB;
+ALTER TABLE trq_nodeproperty ENGINE=INNODB;
+ALTER TABLE trq_nodestate ENGINE=INNODB;
+ALTER TABLE trq_queue ENGINE=INNODB;
+ALTER TABLE trq_runningjob ENGINE=INNODB;
+ALTER TABLE trq_subcluster ENGINE=INNODB;
+ALTER TABLE trq_submithost ENGINE=INNODB;
+ALTER TABLE trq_torqueserver ENGINE=INNODB;
+ALTER TABLE trq_user ENGINE=INNODB;
+
+
 CREATE INDEX `trq_job_comp_time` on `trq_job` (`comp_time`);
