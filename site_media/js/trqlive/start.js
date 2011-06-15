57699c2cc60af0869165cb310af9ca7295c7b609
Tomas Kouba <tomas@jikos.cz>
2011-06-15 10:18:09 +0200

    Nodes tab 'overview' shows only subclusters for the right batch server.

--- site_media/js/trqlive/start.js
+++ site_media/js/trqlive/start.js
@@ -70,9 +70,9 @@ Ext.onReady(function () {
         autoLoad: false
     });
 
-/*
- * VIEWS
- */
+    /*
+    * VIEWS
+    */
     Ext.define('Ext.org.NodesView', {
         extend: 'Ext.view.View',
         alias: 'widget.nodesview',
@@ -83,71 +83,8 @@ Ext.onReady(function () {
         tpl: ['<tpl for=".">', '<div data-qtitle="{shortname}" data-qtip="{ttiphtml}" id="overview_id_{name}" class="node_overview {state}">', '</div>', '</tpl>']
     });
 
-    var tabs_nodes = [{
-        title: "Overview",
-        id: "nodes_overview",
-        xtype: 'panel',
-        autoScroll: true,
-        layout: {
-            type: 'hbox',
-            align: 'stretch'
-        },
-        region: 'center',
-        listeners: {
-            activate: function(thistab) {
-                if(thistab.items.getCount()==0) {
-                    subcluster_store.each(function (sc) {
-                        itm = Ext.create('Ext.org.NodesView');
-                        itm.store = Ext.create('Ext.data.Store', {
-                            model: 'Node',
-                            proxy: {
-                                type: 'ajax',
-                                url: '/trqlive/dynamic/nodes_overview/' + sc.get('name') + '/'
-                            },
-                            autoLoad: true,
-                        });
-                        thistab.add({
-                            id: sc.get('name'),
-                            xtype: 'panel',
-                            title: sc.get('name'),
-                            //flex: 1,
-                            width: 100,
-                            items: itm
-                        }
-                        );
-                        tab = thistab.items.get(sc.get('name'));
-                        //console.info(tab.items.get(0).store.proxy);
-/*                        
-                        var operation = new Ext.data.Operation({
-                            action: 'read',
-                            sname : sc.get('name')
-                        });
-*/                        
-//                        tab.items.get(0).store.load();
-/*                        
-                        console.info(itm);
-                        console.info(itm.store);
-                        console.info(itm.store.totalCount);
-                        itm.store.each(function (n) {
-                            Ext.create('Ext.tip.ToolTip', {
-                                target: Ext.getCmp(n),
-                                trackMouse: true,
-                                html: n
-                            });
-                            console.info(n);
-                        });
-*/                        
-                    });
-                }
-            },
-            deactivate: function(thistab) {
-                //console.info('deactivated');
-            }
-        }
-    }, {
-        title: "List",
-        id: "nodes_list"
-    }];
+    var tabs_nodes = {};
+
 
     var tp_store = Ext.create('Ext.data.TreeStore', {
         root: {
@@ -172,26 +109,24 @@ Ext.onReady(function () {
             itemclick: function (view, rec, item, index, eventObj) {
                 var mp = Ext.getCmp('main-panel');
                 mp.removeAll();
-                console.info(rec);
-                console.info(rec.parentNode);
-                console.info(rec.parentNode.get('text'));
                 if (rec.get('text') == 'Nodes') {
-                    Ext.Array.each(tabs_nodes, function (tab) {
+                    Ext.Array.each(tabs_nodes[rec.parentNode.get('text')], function (tab) {
+                        console.info(tab);
                         mp.add(tab);
                     });
                 }
-/*
-                                    var curtab = Ext.getCmp('tab_'+rec.get('id'));
-                                    if(!curtab) {
-                                    curtab = {
-                                    id: 'tab_'+rec.get('id'),
-                                    title: rec.get('id'),
-                                    html: '<b>' + rec.get('id') + '</b>'
-                                    };
-                                    curtab = mp.add(curtab);
-                                    }
-                                    mp.setActiveTab(curtab);
-                                    */
+                /*
+                var curtab = Ext.getCmp('tab_'+rec.get('id'));
+                if(!curtab) {
+                curtab = {
+                id: 'tab_'+rec.get('id'),
+                title: rec.get('id'),
+                html: '<b>' + rec.get('id') + '</b>'
+                };
+                curtab = mp.add(curtab);
+                }
+                mp.setActiveTab(curtab);
+                */
             }
         },
         store: tp_store
@@ -199,32 +134,75 @@ Ext.onReady(function () {
     });
     batchserver_store.on('load', function (store, records, successful) {
         var root = tp_store.getRootNode();
-        Ext.Array.each(records, function(rec) {
+        Ext.Array.each(records, function(bs) {
             root.appendChild({
-                text: rec.get('name'),
+                text: bs.get('name'),
                 expanded: true,
                 children: [{
                     text: "Queues",
                     leaf: true,
-                    id: "queues_"+rec.get('name')
+                    id: "queues_"+bs.get('name')
                 }, {
                     text: "Nodes",
                     leaf: true,
-                    id: "nodes_"+rec.get('name')
+                    id: "nodes_"+bs.get('name')
                 }, {
                     text: "Users",
                     leaf: true,
-                    id: "users_"+rec.get('name')
+                    id: "users_"+bs.get('name')
                 }]
             });
-            subcluster_stores[rec.get('name')] = Ext.create('Ext.data.Store', {
+            subcluster_stores[bs.get('name')] = Ext.create('Ext.data.Store', {
                 model: 'Subcluster',
                 proxy: {
                     type: 'ajax',
-                    url: '/trqlive/dynamic/subclusters_list/'+rec.get('name')+'/',
+                    url: '/trqlive/dynamic/subclusters_list/'+bs.get('name')+'/',
                 },
-                autoLoad: true
+                autoLoad: true,
             });
+            tabs_nodes[bs.get('name')] = [{
+                title: "Overview",
+                id: "nodes_overview",
+                xtype: 'panel',
+                autoScroll: true,
+                layout: {
+                    type: 'hbox',
+                    align: 'stretch'
+                },
+                region: 'center',
+                listeners: {
+                    activate: function(thistab) {
+                        if(thistab.items.getCount()==0) {
+                            subcluster_stores[bs.get('name')].each(function (sc) {
+                                itm = Ext.create('Ext.org.NodesView');
+                                itm.store = Ext.create('Ext.data.Store', {
+                                    model: 'Node',
+                                    proxy: {
+                                        type: 'ajax',
+                                        url: '/trqlive/dynamic/nodes_overview/' + sc.get('name') + '/'
+                                    },
+                                    autoLoad: true,
+                                });
+                                thistab.add({
+                                    id: sc.get('name'),
+                                    xtype: 'panel',
+                                    title: sc.get('name'),
+                                    //flex: 1,
+                                    width: 100,
+                                    items: itm
+                                }
+                                );
+                            });
+                        }
+                    },
+                    deactivate: function(thistab) {
+                        //console.info('deactivated');
+                    }
+                }
+            }, {
+                title: "List",
+                id: "nodes_list"
+            }];
         });
         console.info("subcluster_stores:");
         console.info(subcluster_stores);
