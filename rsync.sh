d4ee9c89ed4b0c0d967727a199c00b6503e3933f
Tomas Kouba <tomas@jikos.cz>
2011-10-01 18:33:27 +0200

    Download dpm logs in rsync.sh.
    Update atlas month script.

--- rsync.sh
+++ rsync.sh
@@ -7,3 +7,9 @@ rsync -avz root@ce2.egee.cesnet.cz:/var/spool/PBS/server_priv/accounting/ /egee_
 rsync -avz root@cream1.egee.cesnet.cz:/opt/glite/var/log/accounting/ /egee_cesnet/cream1_opt_glite_var_log_accounting
 rsync -avz --delete root@ce2.egee.cesnet.cz:/opt/edg/var/ /egee_cesnet/ce2_opt_edg_var/
 rsync -avz --delete root@ce1.egee.cesnet.cz:/opt/edg/var/ /egee_cesnet/ce1_opt_edg_var/
+
+
+for i in `seq 1 14`; do
+	rsync -avz root@dpmpool${i}.farm.particle.cz:/var/log/dpm-gsiftp/ /dpmpool_logs/dpmpool${i}.farm.particle.cz/dpm-gsiftp/
+	rsync -avz root@dpmpool${i}.farm.particle.cz:/var/log/rfio/ /dpmpool_logs/dpmpool${i}.farm.particle.cz/rfio/
+done
