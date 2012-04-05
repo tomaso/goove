79fb05e0405d500f7af6cea6a285813af9c398d8
Tomas Kouba <tomas@jikos.cz>
2012-04-05 17:37:28 +0200

    Basics of job processing.

---
+++ job_processor.py
@@ -0,0 +1,18 @@
+# Process jobs from accounting events data
+
+def is_processed(job):
+    """ Is the job already processed?
+    """
+    pass
+
+def is_processed(full_jobid):
+    """Is the job already processed?
+    """
+    pass
+    
+def process_job(full_jobid):
+    """ Process given job from accounting data
+    """
+
+
+# vi:ts=4:sw=4:expandtab
